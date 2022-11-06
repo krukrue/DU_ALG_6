a7094f55628818d61657fce6f5c36a477b10484f
krukrue <92872517+krukrue@users.noreply.github.com>
2022-11-06 22:21:55 +0100

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,62 @@
+﻿// Реализация хеш-таблицы в C++
+
+#include <iostream>
+#include <list>
+#include "HashNode.h"
+using namespace std;
+
+
+int main()
+{
+    int key[] = { 1, 2, 3, 4, 5, 6, 5 };
+    int data[] = {18,  123, 432, 523, 43, 423, 111 };
+    int size = sizeof(key) / sizeof(key[0]);
+
+
+    SeparateChaining <int> h(size);
+
+    for (int i = 0; i < size; i++)
+        h.insert(key[i], data[i]);
+    h.Delete(1);
+    h.Delete(5);
+
+
+    h.PrintStructure();
+
+    int key1[] = { 1, 2, 3, 4, 3, 5, 2, 7, 8};
+    char data1[] = { 'a','b','c','d','r','2','l','u','i' };
+    int size1 = sizeof(key1) / sizeof(key1[0]);
+    
+    SeparateChaining <char> c(size1);
+    
+    for (int i = 0; i < size1; i++)
+        c.insert(key1[i], data1[i]);
+    c.Delete(1);
+    c.Delete(2);
+    cout << "Search Result: " << c.search(4) << endl;
+    
+    c.PrintStructure();
+
+    float data2[] = { 17.7, 12.6, 1.1, 2.2, 3.3, 4.4, 5.5,6.6,7.7,8.8, 9.9, 10.1, 11.2, 13.2, 14.6};
+    int key2[] = { 1,  3, 2, 4, 6, 2, 4, 5, 8, 22, 12, 7, 8, 9, 0 };
+    int size2 = sizeof(key2) / sizeof(key2[0]);
+
+
+    SeparateChaining <float> s(size2);
+
+    for (int i = 0; i < size2; i++)
+        s.insert(key2[i], data2[i]);
+    cout << "Search Result: " << s.search(1) << endl;
+    cout << "Search Result: " << s.search(5) << endl;
+    cout << "Search Result: " << s.search(22) << endl;
+    cout << "Search Result: " << s.search(2) << endl;
+    cout << "Search Result: " << s.search(3) << endl;
+
+    s.Delete(1);
+    s.Delete(5);
+    s.Delete(22);
+    s.Delete(2);
+    s.Delete(3);
+
+    s.PrintStructure();
+}
---
+++ ConsoleApplication6/HashNode.h
@@ -0,0 +1,105 @@
+#pragma once
+#include <iostream>
+#include <list>
+using namespace std;
+
+template<typename T>
+class SeparateChaining
+{
+public:
+    int capacity;
+    list<T>* table; // misto struct HashTable
+
+    SeparateChaining(int V);
+    void insert(int key, T data);
+    void Delete(int key);
+    T search(int key);
+    bool isSet(int n) // kontrolujeme zda byl list nainizilizovan
+    {
+        int i;
+        if (n == 1 || n == 0)
+        {
+            return false;
+        }
+        for (i = 2; i < n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    int getSet(int n)
+    {
+        if (n % 2 == 0)
+        {
+            n++;
+        }
+        while (!isSet(n))
+        {
+            n += 2;
+        }
+        return n;
+    }
+
+    int hashFunction(int key)
+    {
+        return (key % capacity); // bereme index pro nas prvek metodou deleni klice na kapacitu
+    }
+    void PrintStructure();
+};
+
+template<typename T>
+SeparateChaining <T>::SeparateChaining(int c) // konstruktor 
+{
+    int size = getSet(c);  // velikost 
+    this->capacity = size; // kapacita
+    table = new list<T>[capacity]; // novy list
+}
+
+template<typename T>
+void SeparateChaining <T>::insert(int key, T data)
+{
+    int index = hashFunction(key); // bereme index nasiho prvku
+    table[index].push_back(data); // push podle indexu
+}
+
+template<typename T>
+void SeparateChaining <T>::Delete(int key) 
+{
+    int index = hashFunction(key); // bereme index nasiho prvku
+
+    for (auto i = table[index].begin();
+        i != table[index].end(); i++)
+    {
+        table[index].erase(i); // odstranime retezec z listu
+        break;
+
+    }
+
+}
+
+
+template<typename T>
+void SeparateChaining <T>::PrintStructure() // vypis kazdeho prvku
+{
+    for (int i = 0; i < capacity; i++)
+    {
+        cout << "table[" << i << "]";
+        for (auto x : table[i])
+            cout << " --> " << x;
+        cout << endl;
+    }
+}
+template<typename T>
+inline T SeparateChaining<T>::search(int key) // plny pruchod list a porovnani s klicem, ktery hledame
+{
+    for (int i = 0; i < capacity; i++)
+    {
+        for (auto x : table[i])
+            if (key == i) {
+                return x;
+            }
+    }
+}
